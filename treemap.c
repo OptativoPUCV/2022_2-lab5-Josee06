ee97b1dece3fd4c0b8e8a0f6e3e43bf7459b3351
Replit user <>
2022-09-26 02:23:59 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -99,24 +99,67 @@ TreeNode * minimum(TreeNode * x)
 
 void removeNode(TreeMap * tree, TreeNode* node) 
 {
-  if(node->right==NULL && node->left==NULL)
-  {
-    if(tree->root!=node)
-    {
-      if(node->parent->right==node)
-      {
-        node->parent->right=NULL;
-      }
-      else
-      {
-        node->parent->left=NULL;
-      }
+   if(node->right == NULL && node->left == NULL){
+        if(node == tree->root){
+            tree->root = NULL;
+            tree->current = NULL;
+        }
+        else if(node->parent->right == node)
+            node->parent->right = NULL;
+        else
+            node->parent->left = NULL;
     }
-    else
-    {
-      tree->current=NULL;
+        
+
+    else if(node -> right == NULL || node -> left == NULL){
+        
+        if(node == tree->root){
+            
+            
+            if(node->left != NULL){
+            tree->root = node->left;
+            node->left->parent = NULL;
+        }
+        
+        // hijo derecho
+        else{
+            tree->root = node->right;
+            node->right->parent = NULL;
+        }
     }
-  }
+    
+    
+        else if(node == node->parent->left){
+      
+        
+            if(node->left != NULL){
+                node->left->parent = node->parent;
+                node->parent->left = node->left;
+            }
+ 
+            else{
+                node->right->parent = node->parent;
+                node->parent->right = node->right;
+            }
+        }
+        else{
+            if(node->left != NULL){
+                node->parent->right = node->left;
+                node->left->parent = node->parent;
+            }
+            else{
+                node->parent->right = node->right;
+                node->right->parent = node->parent;
+            }
+        }
+    }
+    else{
+        TreeNode *auxNode = minimum(node->right);
+        node->pair=auxNode->pair;
+        removeNode(tree, auxNode);
+        tree->current = node;
+    }
+}
 }
 
 void eraseTreeMap(TreeMap * tree, void* key)
